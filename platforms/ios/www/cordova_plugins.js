83614a111afd4f084b27e10657c6fc36b28c8ae7
Madam <oyamamadam@hotmail.com>
2017-04-17 13:23:09 -0700

    Remove fem

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "cordova-plugin-fcm.FCMPlugin",
-        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
-        "pluginId": "cordova-plugin-fcm",
-        "clobbers": [
-            "FCMPlugin"
-        ]
-    },
-    {
         "id": "cordova-plugin-statusbar.statusbar",
         "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
         "pluginId": "cordova-plugin-statusbar",
@@ -20,7 +12,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-fcm": "2.1.1",
     "cordova-plugin-statusbar": "2.2.3-dev",
     "cordova-plugin-whitelist": "1.3.2"
 };
